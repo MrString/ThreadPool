60ff3a5cca622edf3a61fc01edcab45a3a770979
MrString <627271551@qq.com>
2017-04-26 14:29:59 +0800

    start

--- threadpool.h
+++ threadpool.h
@@ -15,6 +15,19 @@ public:
 	threadpool(int thread_number = 8, int max_requests = 10000);
 	~threadpool();
 	bool append(T* request);
+	
+private:
+	static void* worker(void* arg)
+	void run();
+	
+private:
+	int m_thread_number;    //线程数
+	int m_max_requests;     //最大请求数
+	pthread_t* m_threads;   //线程id
+	std::list<T* > m_workqueue; //请求队列
+	locker m_queuelocker;
+	sem m_queuestat;
+	bool m_stop;
 }
 
 #endif
